46cc254b020150369639069bc76d5decaff8d203
Sebastian Spreizer <spreizer@web.de>
2021-06-25 11:35:33 +0200

    Update app to v3.0.0

---
+++ app/precache-manifest.23caa132df0bf4bd6897e59f7407590c.js
@@ -0,0 +1,230 @@
+self.__precacheManifest = [
+  {
+    "revision": "89fa0a919ee421dc7974",
+    "url": "/css/app.de32ba76.css"
+  },
+  {
+    "revision": "89fa0a919ee421dc7974",
+    "url": "/js/app.157d1d81.js"
+  },
+  {
+    "revision": "ae2398c6c29e7740bfab",
+    "url": "/css/chunk-170938e8.0beb7fec.css"
+  },
+  {
+    "revision": "ae2398c6c29e7740bfab",
+    "url": "/js/chunk-170938e8.bb696c3e.js"
+  },
+  {
+    "revision": "0ff55db4629b4c48e66a",
+    "url": "/js/chunk-2d0b3289.d17b3dc9.js"
+  },
+  {
+    "revision": "f0de14dab92fe4715d10",
+    "url": "/css/chunk-58f061a3.84107dde.css"
+  },
+  {
+    "revision": "f0de14dab92fe4715d10",
+    "url": "/js/chunk-58f061a3.4f6fa6f0.js"
+  },
+  {
+    "revision": "7961267d1945de323252",
+    "url": "/css/chunk-d612be78.85156821.css"
+  },
+  {
+    "revision": "7961267d1945de323252",
+    "url": "/js/chunk-d612be78.71580d86.js"
+  },
+  {
+    "revision": "b66ab4c03eb5e60f5daa",
+    "url": "/css/chunk-d945a6be.82951462.css"
+  },
+  {
+    "revision": "b66ab4c03eb5e60f5daa",
+    "url": "/js/chunk-d945a6be.05f1e85f.js"
+  },
+  {
+    "revision": "be688454b034c5d41793",
+    "url": "/css/chunk-vendors.8c78c484.css"
+  },
+  {
+    "revision": "be688454b034c5d41793",
+    "url": "/js/chunk-vendors.9cefc6d7.js"
+  },
+  {
+    "revision": "eefe4f8f61222e7f4b6b740180e83138",
+    "url": "/img/Rainbow.eefe4f8f.png"
+  },
+  {
+    "revision": "b99c60c33be0eee016ae3efc83662612",
+    "url": "/img/Plasma.b99c60c3.png"
+  },
+  {
+    "revision": "7a44ea195f395e1d086010e44555a5c4",
+    "url": "/fonts/materialdesignicons-webfont.7a44ea19.woff2"
+  },
+  {
+    "revision": "147e3378b44bc9570418b1eece10dd7c",
+    "url": "/fonts/materialdesignicons-webfont.147e3378.woff"
+  },
+  {
+    "revision": "64d4cf64afd77a4ad2713f648eb920e6",
+    "url": "/fonts/materialdesignicons-webfont.64d4cf64.eot"
+  },
+  {
+    "revision": "174c02fc4609e8fc4389f5d21f16a296",
+    "url": "/fonts/materialdesignicons-webfont.174c02fc.ttf"
+  },
+  {
+    "revision": "08d91bc0631aaea87db13c723a1d5c7b",
+    "url": "/img/nest-desktop-logo.08d91bc0.png"
+  },
+  {
+    "revision": "ed429366c49ee1688c35d4fc9362ee83",
+    "url": "/img/CubehelixDefault.ed429366.png"
+  },
+  {
+    "revision": "89fc54c9a385095d8f55d3903f838c27",
+    "url": "/img/eu-logo.89fc54c9.png"
+  },
+  {
+    "revision": "783528e4a247a513abc093d05f4571e0",
+    "url": "/img/Viridis.783528e4.png"
+  },
+  {
+    "revision": "59e02c461981a85f4cfaed7868580266",
+    "url": "/img/Inferno.59e02c46.png"
+  },
+  {
+    "revision": "12f86949adc96fa4f7f6e45f81489d44",
+    "url": "/img/nest-simulated.12f86949.png"
+  },
+  {
+    "revision": "8ce61704b5f567af4f7c82990f297eb3",
+    "url": "/img/ebrains-logo.8ce61704.png"
+  },
+  {
+    "revision": "c77da7bccf1b8769dfa2f8874e3be8e1",
+    "url": "/img/hbp-logo.c77da7bc.jpeg"
+  },
+  {
+    "revision": "ad538a69b0e8615ed0419c4529344ffc",
+    "url": "/fonts/Roboto-Thin.ad538a69.woff2"
+  },
+  {
+    "revision": "d3b47375afd904983d9be8d6e239a949",
+    "url": "/fonts/Roboto-Thin.d3b47375.woff"
+  },
+  {
+    "revision": "5b4a33e176ff736a74f0ca2dd9e6b396",
+    "url": "/fonts/Roboto-ThinItalic.5b4a33e1.woff2"
+  },
+  {
+    "revision": "8a96edbbcd9a6991d79371aed0b0288e",
+    "url": "/fonts/Roboto-ThinItalic.8a96edbb.woff"
+  },
+  {
+    "revision": "d26871e8149b5759f814fd3c7a4f784b",
+    "url": "/fonts/Roboto-Light.d26871e8.woff2"
+  },
+  {
+    "revision": "c73eb1ceba3321a80a0aff13ad373cb4",
+    "url": "/fonts/Roboto-Light.c73eb1ce.woff"
+  },
+  {
+    "revision": "e8eaae902c3a4dacb9a5062667e10576",
+    "url": "/fonts/Roboto-LightItalic.e8eaae90.woff2"
+  },
+  {
+    "revision": "13efe6cbc10b97144a28310ebdeda594",
+    "url": "/fonts/Roboto-LightItalic.13efe6cb.woff"
+  },
+  {
+    "revision": "73f0a88bbca1bec19fb1303c689d04c6",
+    "url": "/fonts/Roboto-Regular.73f0a88b.woff2"
+  },
+  {
+    "revision": "35b07eb2f8711ae08d1f58c043880930",
+    "url": "/fonts/Roboto-Regular.35b07eb2.woff"
+  },
+  {
+    "revision": "4357beb823a5f8d65c260f045d9e019a",
+    "url": "/fonts/Roboto-RegularItalic.4357beb8.woff2"
+  },
+  {
+    "revision": "f5902d5ef961717ed263902fc429e6ae",
+    "url": "/fonts/Roboto-RegularItalic.f5902d5e.woff"
+  },
+  {
+    "revision": "90d1676003d9c28c04994c18bfd8b558",
+    "url": "/fonts/Roboto-Medium.90d16760.woff2"
+  },
+  {
+    "revision": "1d6594826615607f6dc860bb49258acb",
+    "url": "/fonts/Roboto-Medium.1d659482.woff"
+  },
+  {
+    "revision": "13ec0eb5bdb821ff4930237d7c9f943f",
+    "url": "/fonts/Roboto-MediumItalic.13ec0eb5.woff2"
+  },
+  {
+    "revision": "83e114c316fcc3f23f524ec3e1c65984",
+    "url": "/fonts/Roboto-MediumItalic.83e114c3.woff"
+  },
+  {
+    "revision": "b52fac2bb93c5858f3f2675e4b52e1de",
+    "url": "/fonts/Roboto-Bold.b52fac2b.woff2"
+  },
+  {
+    "revision": "50d75e48e0a3ddab1dd15d6bfb9d3700",
+    "url": "/fonts/Roboto-Bold.50d75e48.woff"
+  },
+  {
+    "revision": "94008e69aaf05da75c0bbf8f8bb0db41",
+    "url": "/fonts/Roboto-BoldItalic.94008e69.woff2"
+  },
+  {
+    "revision": "4fe0f73cc919ba2b7a3c36e4540d725c",
+    "url": "/fonts/Roboto-BoldItalic.4fe0f73c.woff"
+  },
+  {
+    "revision": "59eb3601394dd87f30f82433fb39dd94",
+    "url": "/fonts/Roboto-Black.59eb3601.woff2"
+  },
+  {
+    "revision": "313a65630d341645c13e4f2a0364381d",
+    "url": "/fonts/Roboto-Black.313a6563.woff"
+  },
+  {
+    "revision": "f75569f8a5fab0893fa712d8c0d9c3fe",
+    "url": "/fonts/Roboto-BlackItalic.f75569f8.woff2"
+  },
+  {
+    "revision": "cc2fadc3928f2f223418887111947b40",
+    "url": "/fonts/Roboto-BlackItalic.cc2fadc3.woff"
+  },
+  {
+    "revision": "6fec8e0542f78a2900f0150f3aa99d8c",
+    "url": "/img/Magma.6fec8e05.png"
+  },
+  {
+    "revision": "c35ef3eb40fd03c291890f1eb11c903c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "960b3c7a0c476f7850415022f8825c25",
+    "url": "/img/nest-desktop-icon.svg"
+  },
+  {
+    "revision": "e82ec9eeff7e9aab1907e16ab98cd288",
+    "url": "/img/nest-desktop-logo-scaled.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "08d91bc0631aaea87db13c723a1d5c7b",
+    "url": "/img/nest-desktop-logo.png"
+  }
+];
